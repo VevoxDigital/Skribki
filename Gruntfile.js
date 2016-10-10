6e103e2e250e9148265d0ab2864f115625525b84
Matthew Struble <matt@vevox.io>
2016-10-09 21:55:47 -0700

    Grunt now cleans things

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,15 @@ exports = module.exports = grunt => {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-
+    clean: {
+      wiki: ['./wiki'],
+      logs: ['./logs/*'],
+      dist: ['./dist']
+    }
 
   });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   grunt.registerTask('test', []);
 };
