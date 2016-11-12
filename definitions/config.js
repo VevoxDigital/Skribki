7ee255c3f31908e398122e9b67a6804e4005b69e
Matthew Struble <matt@vevox.io>
2016-11-12 12:51:38 -0800

    Added @config helper

--- definitions/config.js
+++ definitions/config.js
@@ -8,6 +8,10 @@ function initConfig() {
   if (!nconf.$navbar) nconf.argv().env().file({ file: configFile });
 }
 
+F.helpers.config = key => {
+  return nconf.get(key);
+}
+
 F.middleware('navbar', (req, res, next, options, controller) => {
 
   if (controller) {
