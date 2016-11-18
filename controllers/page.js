e9a9bff8dc43de28e8b86705fae9510181ee3b45
Matthew Struble <matt@vevox.io>
2016-11-17 18:31:49 -0800

    Added edit action, needs view population

--- controllers/page.js
+++ controllers/page.js
@@ -23,61 +23,78 @@ function process_page() {
 
   if (method === 'GET') {
 
-    F.model('page').read(self.url, (err, c) => {
-      if (err) return res.status(500).send(err.stack); // TODO Better error handling.
+    if (self.query.a === 'history') {
 
-      let content = c || {
-        title: 'Page Not Found',
-        desc: 'The page you requested was not found.',
-        categories: [],
-        body: `# Page Does not Exist\nWould you like to [create this page](${self.url}?edit)?`
-      };
+    } else if (self.query.a === 'delete') {
 
-      self.repository.title = content.title.toString();
-      self.repository.desc = content.desc.toString();
-      self.repository.categories = content.categories;
+    } else F.model('page').read(self.url, (err, c) => {
+      if (err) return F.response500(self.req, self.res, err);
 
-      if (content.body) {
-        // Is page
-        const parsersDir = path.join(__dirname, '..', 'extensions', 'parsers');
+      if (self.query.a === 'edit') {
+        if (!self.user) return self.redirect(self.url);
 
-        // TODO Cache parsed files.
-        let parsers = [];
-        fs.walk(parsersDir)
-          .on('data', item => {
-            if (item.stats.isFile() && path.extname(item.path) === '.js')
-              parsers.push(require(item.path));
-          }).on('end', () => {
+        let model = { };
+        model.isEdit = !!c;
+        model.content = c ? c.body : '$title Page Title\n$desc Page Desc\n$categories []';
+        model.url = self.url;
 
-            parsers.reduce((cur, next) => { return cur.then(next); }, q(content.body)).then((body) => {
+        self.repository.title = (c ? 'Editing' : 'Creating') + ' Page';
+        self.repository.desc = `${c ? 'Editing' : 'Creating'} '${self.url}' as ${self.user.name || self.user.username}`;
 
-              self.repository.toc = [];
+        self.view('edit', model);
+      } else {
 
-              let headerPattern = /<h([123])[^>]*>(.+)<\/h[123]>/igm, m;
-              while (m = headerPattern.exec(body)) {
-                self.repository.toc.push({
-                  tagS: '<h' + m[1] + '>',
-                  tagE: '</h' + m[1] + '>',
-                  title: m[2],
-                  hash: m[2].toLowerCase().replace(/[^a-z0-9]/ig, '-')
-                });
-              }
+        let content = c || {
+          title: 'Page Not Found',
+          desc: 'The page you requested was not found.',
+          categories: [],
+          body: `# Page Does not Exist\nWould you like to [create this page](${self.url}?a=edit)?`
+        };
 
-              return body;
+        self.repository.title = content.title.toString();
+        self.repository.desc = content.desc.toString();
+        self.repository.categories = content.categories;
 
-            }).then((body) => {
-              self.view('page', { body: body, url: self.url });
-            }).catch(err => {
-              self.throw500(err);
-            }).done();
+        if (content.body) {
+          // Is page
+          const parsersDir = path.join(__dirname, '..', 'extensions', 'parsers');
 
-          });
-      }
-      else {
-        // Is dir
-        self.res.send('is dir ' + content.title + ' ' + content.desc + '<br>' + self.url);
+          // TODO Cache parsed files.
+          let parsers = [];
+          fs.walk(parsersDir)
+            .on('data', item => {
+              if (item.stats.isFile() && path.extname(item.path) === '.js')
+                parsers.push(require(item.path));
+            }).on('end', () => {
+
+              parsers.reduce((cur, next) => { return cur.then(next); }, q(content.body)).then((body) => {
+
+                self.repository.toc = [];
+
+                let headerPattern = /<h([123])[^>]*>(.+)<\/h[123]>/igm, m;
+                while (m = headerPattern.exec(body)) {
+                  self.repository.toc.push({
+                    tagS: '<h' + m[1] + '>',
+                    tagE: '</h' + m[1] + '>',
+                    title: m[2],
+                    hash: m[2].toLowerCase().replace(/[^a-z0-9]/ig, '-')
+                  });
+                }
+
+                return body;
+
+              }).then((body) => {
+                self.view('page', { body: body, url: self.url });
+              }).catch(err => {
+                self.throw500(err);
+              }).done();
+
+            });
+        } else {
+          // Is dir
+          self.res.send('is dir ' + content.title + ' ' + content.desc + '<br>' + self.url);
+        }
       }
     });
-
   } else self.res.send(`method ${method} not supported for ${self.url}`);
 };
