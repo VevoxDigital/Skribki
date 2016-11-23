fc55ae5ab79137128a42654a53003cf173592c02
Matthew Struble <matt@vevox.io>
2016-11-22 17:53:00 -0800

    Added a request debug output

---
+++ definitions/misc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('colors');
+
+// Create a middleware that will debug output all of the incoming requeusts.
+F.middleware('request-debug', (req, res, next, options, controller) => {
+  res.on('finish', () => {
+    let color = 'white';
+    if (res.statusCode >= 500) color = 'red';
+    else if (res.statusCode >= 400) color = 'yellow';
+    else if (res.statusCode >= 300) color = 'cyan';
+    else if (res.statusCode >= 200) color = 'green';
+    LOG.log('verbose', `${req.connection.remoteAddress} ${req.method.bold} ${req.url} ${res.statusCode.toString()[color]}`);
+  });
+  next();
+});
+F.use('request-debug');
