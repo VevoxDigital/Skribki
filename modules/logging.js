ee743efe021ea8f5439e51c39f29299fd37408a5
Matthew Struble <matt@vevox.io>
2016-11-22 17:51:49 -0800

    Added logging system

---
+++ modules/logging.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const winston = require('winston'),
+      path    = require('path');
+require('winston-daily-rotate-file');
+
+exports.id = 'logging';
+
+exports.install = () => {
+
+  F.console = new winston.Logger({
+    level: 'debug',
+    transports: [
+      new winston.transports.Console({
+        colorize: true
+      }),
+      new winston.transports.DailyRotateFile({
+        name: 'info-file',
+        filename: path.join(__dirname, '..', 'logs', 'log'),
+        datePattern: 'info-yy-MM-dd.',
+        prepend: true,
+        level: 'info',
+        json: false
+      }),
+      new winston.transports.DailyRotateFile({
+        name: 'err-file',
+        filename: path.join(__dirname, '..', 'logs', 'log'),
+        datePattern: 'err-yy-MM-dd.log',
+        prepend: true,
+        level: 'warn',
+        json: false
+      })
+    ]
+  });
+
+  global.LOG = F.console;
+  F.log = F.console;
+
+};
