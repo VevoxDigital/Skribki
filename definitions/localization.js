d7f6e7d4b2f629a4d02b5aeca36252fb3e7f65bd
Matthew Struble <matt@vevox.io>
2016-12-11 15:52:51 -0800

    Additional resource file and fixes

--- definitions/localization.js
+++ definitions/localization.js
@@ -4,6 +4,6 @@ const COOKIE = '__lang';
 
 // fetch the locale from the session, default to English
 F.onLocate = req => {
-  req.lang = req.cookie(COOKIE) || 'default';
+  req.lang = req.cookie(COOKIE) || 'en';
   return req.lang;
 };
