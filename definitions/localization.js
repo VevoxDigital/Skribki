9014a527531c18b962ab119ee59da9308da0ee74
Matthew Struble <matt@vevox.io>
2016-12-11 16:20:24 -0800

    Default language now configurable

--- definitions/localization.js
+++ definitions/localization.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const config = require('nconf');
+
 const COOKIE = '__lang';
 
 // fetch the locale from the session, default to English
 F.onLocate = req => {
-  req.lang = req.cookie(COOKIE) || 'en';
+  req.lang = req.cookie(COOKIE) || config.get('language') || 'en';
   return req.lang;
 };
