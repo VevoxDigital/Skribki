5238e47088461d3ac87a373d01eebcecb62dc090
Matthew Struble <matt@vevox.io>
2017-01-09 18:09:36 -0800

    editing works

--- controllers/page.js
+++ controllers/page.js
@@ -4,7 +4,10 @@ const MODEL = 'page';
 
 exports.install = () => {
   F.route(r => { return !F.locked(r); }, routePage);
+  F.route(r => { return !F.locked(r); }, editPage, [ 'post' ]);
 };
+
+// TODO Fix and use `response500` instead of `response500`
 
 function routePage() {
   let page = F.model('page');
@@ -16,7 +19,7 @@ function routePage() {
       }).catch(this.response500).done();
       break;
     case 'edit':
-      // TODO Ensure user is logged in
+      if (!this.user) return this.redirect(this.url);
       page.read(this.url).then(data => {
         this.repository.title = F.localize(this.req, 'title.page.edit');
         this.view('edit', { data: data });
@@ -34,4 +37,18 @@ function routePage() {
       }).catch(this.response500).done();
       break;
   }
+}
+
+function editPage() {
+  if (!this.user) return this.throw401('must be logged in');
+  if (!this.body.body) return this.throw400('must have a body');
+
+  F.model('page').write(this.url, {
+    email: this.user.email,
+    name: this.user.name,
+    body: this.body.body,
+    message: this.body.message
+  }).then(() => {
+    this.redirect(this.url);
+  }).catch(this.response500).done();
 }
--- models/page.js
+++ models/page.js
@@ -84,35 +84,37 @@ exports.read = route => {
   });
 };
 
-exports.write = (route, data) => {
+exports.write = (rt, data) => {
   assert.equal(typeof data, typeof { }, 'data should be an object, got ' + typeof data);
   assert.equal(typeof data.name, 'string', 'data should have string name');
   assert.equal(typeof data.email, 'string', 'data should have string email');
   assert.equal(typeof data.body, 'string', 'data should have string body');
   data.body = data.body.replace(/\r(?:\n)?/g, '\n');
 
-  let deferred = q.defer();
+  return exports.workingFile(rt).then(route => {
+    route = route ? route : '.' + exports.normalizePath(rt);
+    if (route === '.') return deferred.reject(new Error('cannot create file at "/"'));
+    let deferred = q.defer();
+    // lock the file to avoid any weird commits with two simultanious edits
+    // wait 2 seconds for other locks to be released
+    lockfile.lock(F.path.wiki(route + '.lck'), { wait: 2000 }, err => {
+      if (err) return deferred.reject(err);
+      fs.writeFile(F.path.wiki(route), data.body, err => {
+        if (err) deferred.reject(err);
 
-  route = exports.normalizePath(route);
-  // lock the file to avoid any weird commits with two simultanious edits
-  // wait 2 seconds for other locks to be released
-  lockfile.lock(F.path.wiki(route + '.lck'), { wait: 2000 }, err => {
-    if (err) return deferred.reject(err);
-    fs.writeFile(F.path.wiki(route), data.body, err => {
-      if (err) deferred.reject(err);
-
-      // file written with new data, commit to git
-      F.repository.add(route.substring(1))
-        .commit(data.message || 'Update ' + route, { '--author': `"${data.name} <${data.email}>"` }, () => {
-          lockfile.unlock(F.path.wiki(route + '.lck'), (err) => {
-            if (err) return deferred.reject(err);
-            deferred.resolve();
+        // file written with new data, commit to git
+        F.repository.add(route)
+          .commit(data.message || 'Update ' + route.substring(1), { '--author': `"${data.name} <${data.email}>"` }, () => {
+            lockfile.unlock(F.path.wiki(route + '.lck'), (err) => {
+              if (err) return deferred.reject(err);
+              deferred.resolve();
+            });
           });
-        });
+      });
     });
-  });
 
-  return deferred.promise;
+    return deferred.promise;
+  });
 };
 
 exports.parseDocument = doc => {
