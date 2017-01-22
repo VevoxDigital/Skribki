8994cb38a6fa52f329a382075525e5b15c954e00
Matthew Struble <matt@vevox.io>
2017-01-22 11:10:12 -0800

    fall back to framework config style

--- definitions/config.js
+++ definitions/config.js
@@ -5,15 +5,3 @@ const pkg = require('../package.json');
 F.config.name = 'Skribki';
 F.config.version = pkg.version;
 F.config.author = pkg.author;
-
-for (const key in F.config)
-  if (F.config.hasOwnProperty(key) && key.match(/\./)) {
-    let keys = key.split(/\./), val = F.config[key];
-    let obj = F.config;
-    for (const k of keys.slice(0, keys.length - 1)) {
-      obj[k] = obj[k] || { };
-      obj = obj[k];
-    }
-    obj[keys[keys.length - 1]] = val;
-    delete F.config[key];
-  }
