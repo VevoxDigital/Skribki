270685efd6e7c51705af3be54c81be2256dbc5e3
Matthew Struble <matt@vevox.io>
2017-01-22 12:18:53 -0800

    config debugger

--- definitions/config.js
+++ definitions/config.js
@@ -5,3 +5,11 @@ const pkg = require('../package.json');
 F.config.name = 'Skribki';
 F.config.version = pkg.version;
 F.config.author = pkg.author;
+
+let oldConfig = CONFIG;
+
+global.CONFIG = key => {
+  let val = oldConfig(key);
+  LOG.debug(`${key}: ${JSON.stringify(val)}`);
+  return val;
+};
