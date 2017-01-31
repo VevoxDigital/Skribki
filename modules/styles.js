28aa6dfb9bbd186b9c28b879d2e52018a1f0c3db
Matthew Struble <matt@vevox.io>
2017-01-30 21:46:56 -0800

    rewrite styles module for theming

---
+++ modules/styles.js
@@ -0,0 +1,117 @@
+'use strict';
+
+const CACHE_PREFIX = 'style'
+
+const sass  = require('node-sass'),
+      fs    = require('fs'),
+      path  = require('path');
+
+/**
+  * Compiles the given content into CSS, with the give dir and `node_modules` as includePaths
+  * @param content The content to compile
+  * @param dir     The directory for includePaths
+  */
+exports.compile = (content, dir) => {
+  if (content === '') return '';
+  try {
+    return sass.renderSync({
+      data: content,
+      outputStyle: 'compressed',
+      includePaths: [ dir, F.path.root('node_modules') ]
+    }).css.toString();
+  } catch (e) {
+    LOG.error('style engine failure');
+    LOG.error(e.stack);
+    throw e;
+  }
+};
+
+/**
+  * Install a file router for `/styles/*` and set up the configs
+  * @this Framework
+  */
+exports.install = () => {
+  F.path.themes = p => F.path.root('themes/' + p);
+  F.config['default-theme'] = 'vevox-blue';
+
+  F.file('/styles/*', compileTarget);
+  F.config['static-accepts']['.scss'] = true;
+
+  F.onCompileCSS = (filename, content) => {
+    return exports.compile(content);
+  };
+};
+
+/**
+  * Compiles the given target into CSS.
+  * @param req          The target request
+  * @param res          The target response
+  * @param isValidation If truthy, simply returns a boolean if the route is valid.
+  * @this Framework
+  */
+function compileTarget(req, res, isValidation) {
+  if (isValidation) return req.url.endsWith('.scss') || req.url.endsWith('.sass');
+  else if (!compileTarget(req, res, true)) return false;
+
+  // try to load from cache if present.
+  let key = CACHE_PREFIX + '_' + req.url.substring(1).slice(0, -5), cached = F.cache.get(key);
+  if (cached) return send(req, res, key, cached);
+
+  let route = req.url.substring('/styles/'.length);
+
+  if (route.startsWith('theme')) compileTargetTheme(req, res, key, route);
+  else sendFile(req, res, key, F.path.public(req.url));
+
+}
+
+/**
+  * Compiles the given target into themed CSS
+  * @param req   The request
+  * @param res   The response
+  * @param key   The cache key
+  * @param route The route, themed.
+  */
+function compileTargetTheme(req, res, key, route) {
+  let theme = F.config['wiki.theme'] || F.config['default-theme'];
+  LOG.debug('theme: ' + theme);
+
+  sendFile(req, res, key, F.path.themes(theme + route.substring('theme'.length)));
+}
+
+/**
+  * Sends the given file as CSS
+  * @param req  The request
+  * @param res  The response
+  * @param key  The cache key
+  * @param file The file to send
+  */
+function sendFile(req, res, key, file) {
+  fs.readFile(file, (err, data) => {
+    if (err)
+      return err.message.startsWith('ENOENT')
+        ? F.response404(req, res)
+        : F.response500(req, res, err);
+
+    try {
+
+      LOG.info('compiling uncached style ' + key);
+      send(req, res, key, exports.compile( data.toString(), path.dirname(file) ));
+
+    } catch (e) {
+      F.response500(req, res, e);
+    }
+  })
+}
+
+/**
+  * Sends the given data as CSS with a 200 into the output
+  * @param req  The request
+  * @param res  The response
+  * @param key  The cache key.
+  * @param data The data to send.
+  */
+function send(req, res, key, data) {
+  F.cache.set(key, data, '10 minutes');
+  F.responseContent(req, res, 200, data, 'text/css');
+  F.stats.response.file++;
+}
